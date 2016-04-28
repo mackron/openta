8eb416ab4778b0eb6635df0f6030f3a447c48fd8
David Reid <mackron@gmail.com>
2016-04-28 14:41:28 +1000

    Small amount of work on loading feature descriptor files.

--- source/ta_features.c
+++ source/ta_features.c
@@ -7,6 +7,111 @@ static uint32_t ta_features_library__hash_string(const char* str)
     return hashlittle(str, strlen(str), 0);
 }
 
+#if 0
+static ta_feature_category ta_parse_feature_category(const char* featureStr)
+{
+    if (_stricmp(featureStr, "heaps") == 0) {
+        return ta_feature_category_heaps;
+    }
+    if (_stricmp(featureStr, "arm_corpses") == 0) {
+        return ta_feature_category_arm_corpses;
+    }
+    if (_stricmp(featureStr, "core_corpses") == 0 || _stricmp(featureStr, "cor_corpses") == 0) {
+        return ta_feature_category_core_corpses;
+    }
+    if (_stricmp(featureStr, "rocks") == 0) {
+        return ta_feature_category_rocks;
+    }
+    if (_stricmp(featureStr, "shrubs") == 0) {
+        return ta_feature_category_shrubs;
+    }
+    if (_stricmp(featureStr, "foliage") == 0) {
+        return ta_feature_category_foliage;
+    }
+    if (_stricmp(featureStr, "trees") == 0) {
+        return ta_feature_category_trees;
+    }
+    if (_stricmp(featureStr, "ruins") == 0 || _stricmp(featureStr, "ruin") == 0) {
+        return ta_feature_category_ruins;
+    }
+    if (_stricmp(featureStr, "scars") == 0) {
+        return ta_feature_category_scars;
+    }
+    if (_stricmp(featureStr, "metal") == 0) {
+        return ta_feature_category_metal;
+    }
+    if (_stricmp(featureStr, "steamvents") == 0) {
+        return ta_feature_category_steamvents;
+    }
+    if (_stricmp(featureStr, "spires") == 0) {
+        return ta_feature_category_spires;
+    }
+    if (_stricmp(featureStr, "gasplants") == 0) {
+        return ta_feature_category_gasplants;
+    }
+    if (_stricmp(featureStr, "anemones") == 0) {
+        return ta_feature_category_anemones;
+    }
+    if (_stricmp(featureStr, "corals") == 0) {
+        return ta_feature_category_corals;
+    }
+    if (_stricmp(featureStr, "aquacorals") == 0) {
+        return ta_feature_category_aquacorals;
+    }
+    if (_stricmp(featureStr, "smudges") == 0) {
+        return ta_feature_category_smudges;
+    }
+    if (_stricmp(featureStr, "tracks") == 0) {
+        return ta_feature_category_tracks;
+    }
+    if (_stricmp(featureStr, "buildings") == 0 || _stricmp(featureStr, "building") == 0) {
+        return ta_feature_category_buildings;
+    }
+    if (_stricmp(featureStr, "machines") == 0) {
+        return ta_feature_category_machines;
+    }
+    if (_stricmp(featureStr, "trucks") == 0) {
+        return ta_feature_category_trucks;
+    }
+    if (_stricmp(featureStr, "cars") == 0) {
+        return ta_feature_category_cars;
+    }
+    if (_stricmp(featureStr, "barriers") == 0) {
+        return ta_feature_category_barriers;
+    }
+    if (_stricmp(featureStr, "nodes") == 0) {
+        return ta_feature_category_nodes;
+    }
+    if (_stricmp(featureStr, "dragonteeth") == 0) {
+        return ta_feature_category_dragonteeth;
+    }
+    if (_stricmp(featureStr, "glyph") == 0) {
+        return ta_feature_category_glyph;
+    }
+    if (_stricmp(featureStr, "plants") == 0) {
+        return ta_feature_category_plants;
+    }
+    if (_stricmp(featureStr, "pipes") == 0) {
+        return ta_feature_category_pipes;
+    }
+    if (_stricmp(featureStr, "holes") == 0) {
+        return ta_feature_category_holes;
+    }
+    if (_stricmp(featureStr, "craters") == 0) {
+        return ta_feature_category_craters;
+    }
+    if (_stricmp(featureStr, "kelp") == 0) {
+        return ta_feature_category_kelp;
+    }
+    if (_stricmp(featureStr, "monuments") == 0) {
+        return ta_feature_category_monuments;
+    }
+
+    printf("Unknown feature category: %s\n", featureStr);
+    return ta_feature_category_unknown;
+}
+#endif
+
 static bool ta_features_library__load_feature(ta_features_library* pLib, const char* featureName, ta_config_obj* pFeatureConfig)
 {
     if (pLib == NULL || pFeatureConfig == NULL) {
@@ -28,8 +133,101 @@ static bool ta_features_library__load_feature(ta_features_library* pLib, const c
     }
 
     ta_feature_desc* pFeature = pLib->pFeatures + pLib->featuresCount;
+    memset(pFeature, 0, sizeof(*pFeature));
     pFeature->hash = ta_features_library__hash_string(featureName);
 
+    for (uint32_t iVar = 0; iVar < pFeatureConfig->varCount; ++iVar)
+    {
+        ta_config_var* pVar = pFeatureConfig->pVars + iVar;
+
+        if (_stricmp(pVar->name, "description") == 0) {
+            strncpy_s(pFeature->description, sizeof(pFeature->description), pVar->value, _TRUNCATE);
+            continue;
+        }
+
+#if 0
+        if (_stricmp(pVar->name, "category") == 0) {
+            pFeature->category = ta_parse_feature_category(pVar->value);
+            continue;
+        }
+#endif
+
+        if (_stricmp(pVar->name, "filename") == 0) {
+            strcpy_s(pFeature->filename, sizeof(pFeature->filename), pVar->value);
+            continue;
+        }
+        if (_stricmp(pVar->name, "seqname") == 0) {
+            strcpy_s(pFeature->seqname, sizeof(pFeature->seqname), pVar->value);
+            continue;
+        }
+        if (_stricmp(pVar->name, "seqnameburn") == 0) {
+            strcpy_s(pFeature->seqnameburn, sizeof(pFeature->seqnameburn), pVar->value);
+            continue;
+        }
+        if (_stricmp(pVar->name, "seqnamedie") == 0) {
+            strcpy_s(pFeature->seqnamedie, sizeof(pFeature->seqnamedie), pVar->value);
+            continue;
+        }
+        if (_stricmp(pVar->name, "seqnamereclamate") == 0) {
+            strcpy_s(pFeature->seqnamereclamate, sizeof(pFeature->seqnamereclamate), pVar->value);
+            continue;
+        }
+        if (_stricmp(pVar->name, "seqnameshad") == 0) {
+            strcpy_s(pFeature->seqnameshadow, sizeof(pFeature->seqnameshadow), pVar->value);
+            continue;
+        }
+        if (_stricmp(pVar->name, "object") == 0) {
+            strcpy_s(pFeature->object, sizeof(pFeature->object), pVar->value);
+            continue;
+        }
+
+
+        // Booleans
+        if (_stricmp(pVar->name, "animating") == 0) {
+            pFeature->flags |= TA_FEATURE_ANIMATING;
+            continue;
+        }
+        if (_stricmp(pVar->name, "animtrans") == 0) {
+            pFeature->flags |= TA_FEATURE_ANIMTRANS;
+            continue;
+        }
+        if (_stricmp(pVar->name, "autoreclaimable") == 0) {
+            pFeature->flags |= TA_FEATURE_AUTORECLAIMABLE;
+            continue;
+        }
+        if (_stricmp(pVar->name, "blocking") == 0) {
+            pFeature->flags |= TA_FEATURE_BLOCKING;
+            continue;
+        }
+        if (_stricmp(pVar->name, "flamable") == 0) {
+            pFeature->flags |= TA_FEATURE_FLAMABLE;
+            continue;
+        }
+        if (_stricmp(pVar->name, "geothermal") == 0) {
+            pFeature->flags |= TA_FEATURE_GEOTHERMAL;
+            continue;
+        }
+        if (_stricmp(pVar->name, "indestructible") == 0) {
+            pFeature->flags |= TA_FEATURE_INDESTRUCTIBLE;
+            continue;
+        }
+        if (_stricmp(pVar->name, "nodisplayinfo") == 0) {
+            pFeature->flags |= TA_FEATURE_NODISPLAYINFO;
+            continue;
+        }
+        if (_stricmp(pVar->name, "permanent") == 0) {
+            pFeature->flags |= TA_FEATURE_PERMANENT;
+            continue;
+        }
+        if (_stricmp(pVar->name, "reclaimable") == 0) {
+            pFeature->flags |= TA_FEATURE_RECLAIMABLE;
+            continue;
+        }
+        if (_stricmp(pVar->name, "shadtrans") == 0) {
+            pFeature->flags |= TA_FEATURE_SHADOWTRANSPARENT;
+            continue;
+        }
+    }
 
 
     // Add the feature to the end of the list.
--- source/ta_features.h
+++ source/ta_features.h
@@ -1,11 +1,109 @@
 // Public domain. See "unlicense" statement at the end of this file.
 
+#define TA_FEATURE_ANIMATING            0x0001
+#define TA_FEATURE_ANIMTRANS            0x0002
+#define TA_FEATURE_AUTORECLAIMABLE      0x0004
+#define TA_FEATURE_BLOCKING             0x0008
+#define TA_FEATURE_FLAMABLE             0x0010
+#define TA_FEATURE_GEOTHERMAL           0x0020
+#define TA_FEATURE_INDESTRUCTIBLE       0x0040
+#define TA_FEATURE_NODISPLAYINFO        0x0080
+#define TA_FEATURE_PERMANENT            0x0100
+#define TA_FEATURE_RECLAIMABLE          0x0200
+#define TA_FEATURE_SHADOWTRANSPARENT    0x0400
+
+#if 0
+// The different feature categories that the game actually uses.
+typedef enum
+{
+    ta_feature_category_unknown,
+    ta_feature_category_heaps,
+    ta_feature_category_arm_corpses,
+    ta_feature_category_core_corpses,
+    ta_feature_category_rocks,
+    ta_feature_category_shrubs,
+    ta_feature_category_foliage,
+    ta_feature_category_trees,
+    ta_feature_category_ruins,
+    ta_feature_category_scars,
+    ta_feature_category_metal,
+    ta_feature_category_steamvents,
+    ta_feature_category_spires,
+    ta_feature_category_gasplants,
+    ta_feature_category_anemones,
+    ta_feature_category_corals,
+    ta_feature_category_aquacorals,
+    ta_feature_category_smudges,
+    ta_feature_category_tracks,
+    ta_feature_category_buildings,
+    ta_feature_category_machines,
+    ta_feature_category_trucks,
+    ta_feature_category_cars,
+    ta_feature_category_barriers,
+    ta_feature_category_nodes,
+    ta_feature_category_dragonteeth,
+    ta_feature_category_glyph,
+    ta_feature_category_plants,
+    ta_feature_category_pipes,
+    ta_feature_category_holes,
+    ta_feature_category_craters,
+    ta_feature_category_kelp,
+    ta_feature_category_monuments,
+} ta_feature_category;
+#endif
+
 // Structure describing a feature. This is not a feature instantiation. The name of the feature is not
 // currently stored for the sake of keeping this structure as small and simple as possible.
+//
+// Note that not every property is stored in this structure - only what we need.
 struct ta_feature_desc
 {
     // The hash representing the feature descriptors name.
     uint32_t hash;  // <-- If collisions becomes an issue when using a 32-bit hash, consider switching to 64-bit.
+
+    // The description of the feature. This is shown at the bottom of the screen when the player places the mouse over the object.
+    char description[64];
+
+#if 0
+    // The category. We're only using a set of predefined categories that we use which means this can be represented
+    // with an enumerator.
+    ta_feature_category category;
+#endif
+
+    // File .GAF file that contains the feature's graphics.
+    char filename[64];
+
+    // The name of the sequence within the GAF file of <filename> where the default graphic is located.
+    char seqname[64];
+
+    // Same as seqname, except for when the feature is burned.
+    char seqnameburn[64];
+
+    // The graphic used after the feature has been destroyed.
+    char seqnamedie[64];
+
+    // The graphic animation used when the feature is reclamated.
+    char seqnamereclamate[64];
+
+    // The graphic used to draw the feature's shadow.
+    char seqnameshadow[64];
+
+    // The 3DO object to use as the graphic for the feature. Blank for 2D features.
+    char object[64];
+
+    // Boolean properties
+    //  TA_FEATURE_ANIMATING -> "animating"
+    //  TA_FEATURE_ANIMTRANS -> "animtrans"
+    //  TA_FEATURE_AUTORECLAIMABLE -> "autoreclaimable"
+    //  TA_FEATURE_BLOCKING -> "blocking"
+    //  TA_FEATURE_FLAMABLE -> "flamable"
+    //  TA_FEATURE_GEOTHERMAL -> "geothermal"
+    //  TA_FEATURE_INDESTRUCTIBLE -> "indestructible"
+    //  TA_FEATURE_NODISPLAYINFO -> "nodisplayinfo"
+    //  TA_FEATURE_PERMANENT -> "permanent"
+    //  TA_FEATURE_RECLAIMABLE -> "reclaimable"
+    //  TA_FEATURE_SHADOWTRANSPARENT -> "shadtrans"
+    uint16_t flags;
 };
 
 // Structure containing the descriptors of every known feature. This structure is filled
