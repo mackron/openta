a512578fdd9e815f9244bc1d5a5fcd30464e8cbc
David Reid <mackron@gmail.com>
2016-11-19 16:56:08 +1000

    Add a few result codes.

--- source/openta/ta_errors.h
+++ source/openta/ta_errors.h
@@ -1,5 +1,11 @@
 // Public domain. See "unlicense" statement at the end of this file.
 
+typedef int ta_result;
+#define TA_SUCCESS           0
+#define TA_ERRORS           -1
+#define TA_INVALID_ARGS     -2
+#define TA_OUT_OF_MEMORY    -3
+
 #define TA_ERROR_FAILED_TO_PARSE_CMDLINE        -1
 #define TA_ERROR_FAILED_TO_CREATE_GAME_CONTEXT  -2
 #define TA_ERROR_INVALID_ARGS                   -3
